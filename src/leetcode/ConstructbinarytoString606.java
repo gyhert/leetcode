390dbe8e866a24cb3780bd56562a7d7f6ff7b65e
pengw00 <water685@hotmail.com>
2018-04-20 23:13:57 -0500

    d

--- src/leetcode/ConstructbinarytoString606.java
+++ src/leetcode/ConstructbinarytoString606.java
@@ -1,4 +1,21 @@
 package leetcode;
 
 public class ConstructbinarytoString606 {
+    public String tree2str(Treenode t){
+        if(t == null){
+            return "";
+        }
+        if(t.left == null && t.right == null){
+            return t.val + "";
+        }
+        if(t.left == null){
+            return t.val + "()" + "(" + tree2str(t.right) + ")";
+        }
+        if(t.right == null){
+            return t.val + "(" + tree2str(t.left) + ")";
+        }
+        if(t.right == null){
+            return t.val + "(" + tree2str(t.left) + ")" + "(" + tree2str(t.right) + ")";
+        }
+    }
 }
